40bc825009ac80f5def93c1fe1269e9adadfdc43
Ronald Joe Reed <rjreed67@gmail.com>
2014-12-23 14:36:31 +0000

    Import of RJREED/Net-Twitch-Oauth2-0.06.tar.gz from CPAN.
    gitpan-cpan-distribution: Net-Twitch-Oauth2
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         RJREED/Net-Twitch-Oauth2-0.06.tar.gz
    gitpan-cpan-author:       RJREED
    gitpan-cpan-maturity:     released

--- lib/Net/Twitch/Oauth2.pm
+++ lib/Net/Twitch/Oauth2.pm
@@ -11,7 +11,7 @@ use Carp;
 use constant ACCESS_TOKEN_URL => 'https://api.twitch.tv/kraken/oauth2/token';
 use constant AUTHORIZE_URL => 'https://api.twitch.tv/kraken/oauth2/authorize';
  
-our $VERSION = '0.05';
+our $VERSION = '0.06';
  
 sub new {
     my ($class,%options) = @_;
